b0471adac9cfc09e740ba273a3337e47bb0e30ea
teejay3 <34775293+teejay3@users.noreply.github.com>
2019-10-04 22:01:26 +0300

    Update payments.js

--- routers/payments.js
+++ routers/payments.js
@@ -1,14 +1,17 @@
+/* eslint-disable strict */
+/* eslint-disable max-len */
+
 'use strict';
 
 const req = require('request');
-const fs = require('fs');
-// const pool = require('../utils/db_connect');
+// const fs = require('fs');
+const logger = require('../utils/logger');
 const dal = require('../utils/dal');
 
 let paymentUrl = 'https://securepayments.sberbank.ru/payment/rest/register.do?token=';
 const successUrl = 'http://cybersirius.ru/success.php?order=';
 let statusUrl = 'https://securepayments.sberbank.ru/payment/rest/getOrderStatusExtended.do?token=';
-const ordersLog = 'orders.log';
+// const ordersLog = 'orders.log';
 
 /* var games =
     [{ 'name': "lol", 'fullName': 'League of legends', 'price': 2500 },
@@ -23,22 +26,12 @@ const payments = [{ type: '0', price: 35000 },
     { type: '3', price: 5000 }];
 
 // запрос к удалённому ресурсу по url
-async function makeRequest(ordId, url)
+async function makeRequest(url)
 {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) =>
+    {
         req.get(url, (error, resp, body) =>
         {
-            const now = new Date();
-            const msg = JSON.parse(body).formUrl || JSON.parse(body).errorCode;
-
-            // let currentUrl = new URL(url);
-            // let query_string = current_url.search;
-            // let search_params = new URLSearchParams(query_string);
-            const searchParams = new URLSearchParams(URL(url).search);
-            const order = searchParams.get('orderNumber');
-
-            const data = `${now}\t${msg}\t${order}`;
-            fs.appendFile(ordersLog, `${data} \n`, () => { });
             if (error === null)
             {
                 resolve(body);
@@ -54,54 +47,59 @@ async function makeRequest(ordId, url)
 // возвращает порядковый номер следующего заказа
 async function getNextOrder(data)
 {
-    // console.log('get next order');
-    // const values = [data.token, data.user_name, data.type, data.amount, new Date()];
     try
     {
-        const result = await dal.getNextOrder(data.token, data.user_name, data.type, data.amount, new Date());
-        // console.log(result);
+        /* const result = await dal.getNextOrder(
+            data.token,
+            data.user_name,
+            data.type,
+            data.amount,
+            new Date(),
+        ); */
+        const result = await dal.smartRouter(
+            getNextOrder.name,
+            [
+                data.token,
+                data.user_name,
+                data.type,
+                data.amount,
+                new Date(),
+            ],
+        );
         // получили номер заказа
-        // const orderNum = parseInt(result[0].f_get_next_order, 10);
-        // console.log(orderNum);
         return parseInt(result[0].f_get_next_order, 10);
     }
     catch (e)
     {
-        console.log(e.stack);
+        logger.error({ message: `Get next order error: ${e.stack}` });
         return -1;
     }
 }
 
 async function updateOrder(ordId, ordSysId)
 {
-    // const values = [ordId, ordSysId, new Date()];
-    try
+    dal.smartRouter(updateOrder.name, [ordId, ordSysId, new Date()]);
+    /* try
     {
-        const result = await dal.updateOrder(ordId, ordSysId, new Date());
-        // console.log(result);
+        await dal.updateOrder([ordId, ordSysId, new Date()]);
     }
     catch (e)
     {
-        console.log(e);
-    }
+        logger.error({ message: `update order error: ${e.stack}` });
+    } */
 }
 
 // Боевая оплата
 async function pay(request, response)
 {
-    // console.log('urls ' + paymentUrl + ' ' + successUrl);
     let amount = 0;
     const data = {
         user_name: '', token: '', amount: '', type: '',
     };
-    // имя плательщика
-    const { name } = request.body;
+    // имя плательщика, токен от Php, тип платежа
+    const { name, token, ptype: type } = request.body;
     data.user_name = name;
-    // тип платежа
-    const type = request.body.ptype;
     data.type = type;
-    // токен от Php
-    const { token } = request.body;
     data.token = token;
     try
     {
@@ -111,17 +109,16 @@ async function pay(request, response)
         const orderNum = await getNextOrder(data);
         if (orderNum > 0)
         {
-            // let succUrl = successUrl + orderNum;
-            // let payUrl = paymentUrl + `&currency=643&language=ru&orderNumber=${orderNum}&amount=${data.amount}&returnUrl=${succUrl}&sessionTimeoutSecs=600`;
             const payUrl = `${paymentUrl}&currency=643&language=ru&orderNumber=${orderNum}&amount=${data.amount}&returnUrl=${successUrl + orderNum}&sessionTimeoutSecs=600`;
-            const bdy = await makeRequest(orderNum, payUrl);
+            const bdy = await makeRequest(payUrl);
             updateOrder(orderNum, JSON.parse(bdy).orderId);
+            logger.warn({ message: `Order update ${orderNum} ${JSON.parse(bdy).orderId}` });
             response.send(bdy);
         }
     }
     catch (e)
     {
-        console.log(e);
+        logger.error({ message: `Payment error: ${e.stack}` });
         response.send({ errorCode: 0, errorText: `Wrong parameters ${e.stack}` });
     }
 }
@@ -160,7 +157,7 @@ async function getOrderStatus(request, response)
     }
     catch (e)
     {
-        console.log(e);
+        logger.error({ message: `Get order status error: ${e.stack}` });
     }
 }
 
