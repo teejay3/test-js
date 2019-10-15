802f020457f36c291ea4c61c50b8c3fc53960b66
teejay3 <34775293+teejay3@users.noreply.github.com>
2019-10-15 09:36:40 +0300

    Update dal.js

--- utils/dal.js
+++ utils/dal.js
@@ -12,6 +12,20 @@ async function smartRouter(route, payload)
 }
 module.exports.smartRouter = smartRouter;
 
+// универсальный маршрут для типовых операций
+async function routerByRegexp(ctx)
+{
+    // let result = await db.by_route(route, payload);
+    try
+    {
+        return db.byRegexp(ctx);
+    }
+    catch (e)
+    {
+        throw new Error(e.stack);
+    }
+}
+module.exports.routerByRegexp = routerByRegexp;
 // заказы, оплата
 /* async function getNextOrder(token, userName, type, amount, date)
 {
