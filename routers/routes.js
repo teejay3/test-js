15c988cbf38dd3b9bf3e08cbc30bb8a2b4399b12
teejay3 <34775293+teejay3@users.noreply.github.com>
2019-10-15 09:34:53 +0300

    Create routes.js

---
+++ routers/routes.js
@@ -0,0 +1,102 @@
+/* eslint-disable strict */
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const comm = require('./common');
+const users = require('./users');
+
+const newsRouter = express.Router();
+
+newsRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+newsRouter.delete('/:id', comm.ctxRouter);
+newsRouter.get('/', comm.ctxRouter);
+newsRouter.post('/', urlencodedParser, comm.ctxRouter);
+
+module.exports.newsRouter = newsRouter;
+
+const eventsRouter = express.Router();
+
+eventsRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+eventsRouter.delete('/:id', comm.ctxRouter);
+eventsRouter.post('/', urlencodedParser, comm.ctxRouter);
+eventsRouter.get('/', comm.ctxRouter);
+
+module.exports.eventsRouter = eventsRouter;
+
+const clubsRouter = express.Router();
+
+clubsRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+clubsRouter.delete('/:id', comm.ctxRouter);
+clubsRouter.post('/', urlencodedParser, comm.ctxRouter);
+clubsRouter.get('/', comm.ctxRouter);
+
+module.exports.clubsRouter = clubsRouter;
+
+const userRouter = express.Router();
+
+userRouter.get('/types', comm.ctxRouter);
+userRouter.get('/roles', comm.ctxRouter);
+userRouter.post('/login', urlencodedParser, users.login);
+userRouter.get('/:id', comm.ctxRouter);
+userRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+userRouter.delete('/:id', comm.ctxRouter);
+userRouter.post('/', urlencodedParser, users.login);
+userRouter.get('/', comm.ctxRouter);
+
+module.exports.userRouter = userRouter;
+
+const groupRouter = express.Router();
+
+groupRouter.get('/:id', comm.ctxRouter);
+groupRouter.put('/:id/student/:studId', urlencodedParser, comm.ctxRouter);
+groupRouter.get('/:id/student/', comm.ctxRouter);
+groupRouter.delete('/:id/student/:studId', comm.ctxRouter);
+groupRouter.delete('/:id', comm.ctxRouter);
+groupRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+groupRouter.post('/', urlencodedParser, comm.ctxRouter);
+groupRouter.get('/', comm.ctxRouter);
+
+module.exports.groupRouter = groupRouter;
+
+const gamesRouter = express.Router();
+
+gamesRouter.get('/:id', comm.ctxRouter);
+gamesRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+gamesRouter.delete('/:id', comm.ctxRouter);
+gamesRouter.post('/', urlencodedParser, comm.ctxRouter);
+gamesRouter.get('/', comm.ctxRouter);
+
+module.exports.gamesRouter = gamesRouter;
+
+const trainersRouter = express.Router();
+
+trainersRouter.get('/:id', comm.ctxRouter);
+trainersRouter.put('/:id', urlencodedParser, comm.ctxRouter);
+trainersRouter.delete('/:id', comm.ctxRouter);
+trainersRouter.post('/', urlencodedParser, comm.ctxRouter);
+trainersRouter.get('/', comm.ctxRouter);
+
+module.exports.trainersRouter = trainersRouter;
+
+const defaultRouter = express.Router();
+defaultRouter.use('/', (request, response) =>
+{
+    response.status(404);
+});
+
+module.exports.defaultRouter = defaultRouter;
+
+const aboutRouter = express.Router();
+aboutRouter.get('/', comm.about);
+aboutRouter.post('/', urlencodedParser, comm.about);
+
+module.exports.aboutRouter = aboutRouter;
+
+// const orderRouter = express.Router();
+// orderRouter.post();
+
+// module.exports.orderRouter = orderRouter;
