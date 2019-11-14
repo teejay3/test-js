1198e02ca5986f8d4f06934aaf3d626fd3e8b3b2
teejay3 <34775293+teejay3@users.noreply.github.com>
2019-11-14 15:02:37 +0300

    Update dal.js

--- utils/dal.js
+++ utils/dal.js
@@ -26,28 +26,3 @@ async function routerByRegexp(ctx)
     }
 }
 module.exports.routerByRegexp = routerByRegexp;
-// заказы, оплата
-/* async function getNextOrder(token, userName, type, amount, date)
-{
-    // const result = await db.getNextOrder(token, userName, type, amount, date);
-    // return result;
-    return db.getNextOrder(token, userName, type, amount, date);
-} */
-
-/* async function updateOrder(values)
-{
-    const result = await db.qquery('SELECT', 'f_update_order', values);
-    return result;
-    // const result = await db.updateOrder(ordId, ordSysId, date);
-    // return result;
-} */
-
-/* async function updateToken(token, ip1, ip2, ua, comm)
-{
-    const result = await db.updateToken(token, ip1, ip2, ua, comm);
-    return result;
-} */
-
-// module.exports.getNextOrder = getNextOrder;
-// module.exports.updateOrder = updateOrder;
-// module.exports.updateToken = updateToken;
